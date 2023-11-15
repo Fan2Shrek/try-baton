41a67e9f2e55eed38b0c8d2f71d306bb977b0910
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-11-15 23:02:28 +0100

    :construction: Starting composer events

--- src/Baton.php
+++ src/Baton.php
@@ -1,7 +1,40 @@
 <?php
 
-namespace App;
+namespace Baton;
 
-class Baton
+use Composer\Script\ScriptEvents;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Plugin\PluginInterface;
+
+class Baton implements EventSubscriberInterface, PluginInterface
 {
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $io->write('Welcome to Baton!');
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        $io->write('Goodbye to Baton!');
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        $io->write('Thanks for using Baton!');
+    }
+
+    public function preAutoloadDump(): void
+    {
+        echo "Hello from Baton!\n";
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => 'preAutoloadDump',
+        ];
+    }
 }
